f123277e46169b353de86253e4bfb4ac214a60dd
hudsonchemistry <82335578+hudsonchemistry@users.noreply.github.com>
2021-04-11 15:31:40 -0400

    Delete questionsList.js

--- scripts/questionsList.js
+++
@@ -1,9 +0,0 @@
-const mainDiv = document.getElementById('add-questions')
-for(let i = 0; i< eggQuestions.length; i++) {
-  for (let j = 0; j< eggQuestions[i].questions.length; j++){
-    let question = document.createElement('p')
-    question.innerHTML = eggQuestions[i].questions[j];
-    mainDiv.appendChild(question);
-  }
-
-}
